7600212272611239163993fc0461f93a2665548f
Andrey <devandrey98@gmail.com>
2024-08-11 15:11:26 -0600

    added main page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
-
+import { Hero } from "@/components/Hero";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Hero />
     </main>
   );
 }
