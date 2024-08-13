a1dcd95d3301bbf77b356bce3f3ad20e16fb9a1c
Andrey <devandrey98@gmail.com>
2024-08-13 17:01:16 -0600

    fixed imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero } from "@/components/Hero";
+import { Hero } from "../components/Hero";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
