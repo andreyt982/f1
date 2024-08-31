27ccbc39d3b77b37d1247eade1de35429ab3ded1
Andrey <devandrey98@gmail.com>
2024-08-31 06:53:23 -0600

    added active style to nav links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,12 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 export function Navbar(): JSX.Element {
   const [navActive, setNavActive] = useState(false);
+  const path = usePathname();
 
   const links = {
     internals: ["Live", "Calendar", "Countdown", "Replays"],
@@ -28,7 +30,7 @@ export function Navbar(): JSX.Element {
         }}
       >
         {navActive ? (
-          <svg className=" text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <svg className=" text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6" />
           </svg>
         ) : (
@@ -47,7 +49,7 @@ export function Navbar(): JSX.Element {
           return (
             <Link
               href={link.toLowerCase()}
-              className="hover:text-red-600"
+              className={`hover:text-red-600 ${link.toLowerCase() == path.slice(1) ? "text-red-600" : "text-white"}`}
               key={index}
               onClick={() => {
                 setNavActive(false);
