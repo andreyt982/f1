f4f2d34a174b34a5caca9978fe7f47b6e28eb795
Andrey <devandrey98@gmail.com>
2024-08-30 22:48:42 -0600

    added loader logic for iframe on live component

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -6,6 +6,12 @@ import { useEffect, useState } from "react";
 export default function Live(): JSX.Element {
   const [windowWidth, setWindowWidth] = useState(0);
   const [activeVideoSource, setActiveVideoSource] = useState("");
+  const [iframeLoaded, setIframeLoaded] = useState(false);
+
+  useEffect(() => {
+    setIframeLoaded(false);
+  }, [activeVideoSource]);
+
   useEffect(() => {
     if (window) {
       setWindowWidth(window.innerWidth);
@@ -47,7 +53,30 @@ export default function Live(): JSX.Element {
           );
         })}
       </div>
-      <iframe width={"100%"} height={"900px"} allowFullScreen src={activeVideoSource} {...(windowWidth < 768 && { sandbox: "allow-scripts" })} allow="autoplay" title="iframe"></iframe>
+
+      {!iframeLoaded && (
+        <span className="flex items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="75px" height="75px" viewBox="0 0 24 24">
+            <path fill="grey" d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity="0.25" />
+            <path fill="red" d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z">
+              <animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12" />
+            </path>
+          </svg>
+        </span>
+      )}
+      <iframe
+      className={!iframeLoaded ? 'hidden' : 'block'}
+        width={"100%"}
+        height={"900px"}
+        allowFullScreen
+        src={activeVideoSource}
+        {...(windowWidth < 768 && { sandbox: "allow-scripts" })}
+        allow="autoplay"
+        title="iframe"
+        onLoad={(e) => {
+          setIframeLoaded(true);
+        }}
+      ></iframe>
     </>
   );
 }
