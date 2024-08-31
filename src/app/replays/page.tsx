086a8358d9228fb14e6e6e3590451e79a1223bea
Andrey <devandrey98@gmail.com>
2024-08-31 06:39:50 -0600

    added intial  component

---
+++ src/app/replays/page.tsx
@@ -0,0 +1,8 @@
+export default function Replays() {
+  return (
+    <div className="replays h-screen w-full text-black flex items-center justify-center flex-col">
+      <h1>still working on it...</h1>
+      <img src="/4.jpg" alt="" />
+    </div>
+  );
+}
